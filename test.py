afe6d0364fcc3c80a7ddaf0a4009804db9d35e75
Felix Semler <felix@felix-desktop>
2021-08-27 17:05:31 +0200

    commit

--- test.py
+++ test.py
@@ -12,28 +12,32 @@ sensor = mpu6050(0x68)
 coeff = [0.98,0.02]
 pitch = 0
 roll = 0
-
+#Variable to store [pitch,roll] every 10 seconds
+perm = [['IntGyrx','IntGyry','pitch','pitch_acc','roll','roll_acc']]
 dt = 0.01 #Sampling rate
 
 def ComplementaryFilter(acc, gyr):
     global pitch
-    global coeff
+    global coeff #FIXME: Dont need roll but yaw
     global roll
     #Integrate gyroscope data
     pitch += gyr[0]*dt
     roll -= gyr[1]*dt
-
+    app=[gyr[0]*dt,gyr[1]*dt,0,0,0,0]
     #Compensate for drift with accelerometer data if the drift is significant
     force_mag = sum([abs(i) for i in acc]) #Data is already normalized, so this is technically total acc
     #Most likely we will be using the 2G range as no higher is required
     #FIXME: values, will depend on sidereal speed
     if 0>force_mag>2*9.81: #Check that value is within two g
-        #Get y-axis
+        #Get y-axis rot
         pitch_acc = math.atan2(acc[1]/acc[2])*180/math.pi
         pitch = pitch*coeff[0]+pitch_acc*coeff[1]
-        #Get x-axis
+        #Get x-axis rot
         roll_acc = math.atan2(acc[0]/acc[2])*180/math.pi
         roll = roll*coeff[0]+roll_acc*coeff[1]
+        app=[gyr[0]*dt,gyr[1]*dt,pitch,pitch_acc,roll,roll_acc]
+    perm.append(app)
+
 
 def return_list(dict):
     x = dict['x']
@@ -41,15 +45,26 @@ def return_list(dict):
     z = dict['z']
     return [x,y,z]
 
-def startTimer():
+def startTimer(lim):
+    if lim:
+        if len(perm) == lim:
+            return 0
     threading.Timer(dt, startTimer).start()
     ComplementaryFilter(return_list(sensor.get_accel_data()), return_list(sensor.get_gyro_data()))
-    print('pitch: {}, roll: {}'.format(pitch, roll))
+    
 
 def main():
     sensor.set_accel_range(sensor.ACCEL_RANGE_2G)
-    startTimer()
-    
+    print('Dont move, data collection start in 2 seconds')
+    time.sleep(2)
+    #Collect acc data without movement
+    startTimer(lim=1000)
+    with open('test_data', 'w') as file:
+        for i in perm:
+            file.writelines(i)
+    '''measure = threading.Thread(group=None, target=startTimer,daemon=True)
+    measure.start()
+    res = input('Press enter after rotating 90 degrees')'''
 
 
 if __name__=='__main__':
