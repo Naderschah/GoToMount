7220c7a19ba8e91427c3e7cbea44f2b775e6bd2f
Felix Semler <felix@felix-desktop>
2021-09-01 16:50:24 +0200

    commit

--- get_data.py
+++ get_data.py
@@ -36,9 +36,7 @@ class IMU(object):
             self.gyro_scaled_z, self.accel_scaled_x, self.accel_scaled_y, \
             self.accel_scaled_z) = self.gyroscope.read_all()
         
-        # return (self.pitch, self.roll, self.gyro_scaled_x, self.gyro_scaled_y, self.gyro_scaled_z, self.accel_scaled_x, self.accel_scaled_y, self.accel_scaled_z)
-        return (self.pitch, self.roll, self.gyro_scaled_x, self.gyro_scaled_y, self.gyro_scaled_z, self.accel_scaled_x, self.accel_scaled_y, self.accel_scaled_z)
-
+        
     def comp_filter(self, current_x, current_y):
         new_pitch = IMU.K * (self.pitch + self.gyro_scaled_x * self.time_diff) + (IMU.K1 * current_x)
         new_roll = IMU.K * (self.roll + self.gyro_scaled_y * self.time_diff) + (IMU.K1 * current_y)
