b759ccf3a68f66be4e0c2d5abf555614720981df
Felix Semler <felix@felix-desktop>
2021-09-01 17:30:15 +0200

    bashline

--- calibration/calibrate_magnetometer.py
+++ calibration/calibrate_magnetometer.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 import subprocess
 
-subprocess.call('./2d-calibration-get-samples')
+subprocess.call('2d-calibration-get-samples')
 
-subprocess.call('./2d-calibration-make-calc magnet-data_20181018_1711.txt')
+subprocess.call('2d-calibration-make-calc magnet-data_20181018_1711.txt')
